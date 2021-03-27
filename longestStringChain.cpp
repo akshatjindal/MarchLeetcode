79df57b600a77d50e3688c8c8dcb045c1febef96
akshat jindal <ynwa@umich.edu>
2021-03-27 14:18:49 -0400

    redid longestStringChain without hashmap. Much less memory usage. Practiced range based for loops.

--- longestStringChain.cpp
+++ longestStringChain.cpp
@@ -1,60 +1,58 @@
-//March 25
-#include <unordered_map> 
+//march 27
 #include <vector>
-#include <string.h>
-#include <algorithm>
+#include <string> 
+
+//giving credit to the following person/solution on leetcode: 
+//https://leetcode.com/problems/longest-string-chain/discuss/638101/C%2B%2B-DP-solution-or-Got-96-time-100-memory
 
 using namespace std; 
 
 class Solution {
-public:
-    int longestStrChain(vector<string>& words) {
-        int max_seen_yet = 0; 
-        unordered_map<string, int> wordToMaxChainTillWord; 
-
-        auto comp = [](string a, string b){
-            return a.size() < b.size();
-        };
-
-        std::sort(words.begin(), words.end(), comp); 
-
-        for(int i = 0; i < words.size(); ++i){
-            bool wordExistsAlreadyInMap = false; 
-            if(wordToMaxChainTillWord.find(words[i]) != wordToMaxChainTillWord.end()){
-                wordExistsAlreadyInMap = true; 
-                continue; 
+    bool isPredecessor(string& first, string& second)const {
+        int firstPtr = 0; 
+        int secondPtr = 0; 
+        int diff = 0; 
+        while(firstPtr < first.length() && secondPtr < second.length()){
+            if(first[firstPtr] == second[secondPtr]){
+                ++firstPtr;
             }
-
-            else if(wordExistsAlreadyInMap == false){
-                int longestChainForThisWordsPredecessors = 0; 
-
-                for(int k = 0; k < words[i].length(); ++k){
-                    //let k be the charac to be removed
-                    string temp = words[i]; 
-                    temp.erase(k,1); 
-                    auto iter = wordToMaxChainTillWord.find(temp); 
-                    if(iter != wordToMaxChainTillWord.end()){
-                        if(iter->second > longestChainForThisWordsPredecessors){
-                            longestChainForThisWordsPredecessors = iter->second; 
-                        }
-                    }
-                }//inner
-
-                wordToMaxChainTillWord.insert(std::make_pair(words[i],longestChainForThisWordsPredecessors + 1));
-                if((longestChainForThisWordsPredecessors + 1) > max_seen_yet){
-                    max_seen_yet = longestChainForThisWordsPredecessors + 1; 
-                }
-            }//elseif
             
-        }//outer
+            ++secondPtr; 
+            diff = secondPtr - firstPtr; 
+            if(diff >= 2){
+                return false; 
+            }
+        }//while loop
 
-        return max_seen_yet; 
+        return true; 
+    }//ispredecessor func
+    int globalMax = 1; 
+public:
+    int longestStrChain(vector<string>& words){
+
+        vector<vector<pair<string, int>>> MaxChainStartingAtWord(16); 
+        
+        for(int i = 0; i < words.size(); ++i){
+            MaxChainStartingAtWord[words[i].length() - 1].push_back(std::make_pair(words[i],1));
+        } 
+
+        for(int i = 16 - 2; i >= 0; --i){
+            for(auto& pairOne: MaxChainStartingAtWord[i]){
+                int highestChainLengthForThisWord = 1; 
+                for(auto& pairTwo: MaxChainStartingAtWord[i+1]){
+                    if(isPredecessor(pairOne.first, pairTwo.first) == true){
+                        highestChainLengthForThisWord = 
+                            max(highestChainLengthForThisWord, pairTwo.second + 1); 
+                        pairOne.second = highestChainLengthForThisWord; 
+                        globalMax = max(highestChainLengthForThisWord, globalMax); 
+                    }
+                }//innermost
+            }//inner
+        }//outermost 
+
+        return globalMax; 
     }//func
 };
 
+//Akshat Jindal
 
-int main(){
-    vector<string> kosi = {"a","b","ba","bca","bda","bdca"}; 
-    Solution x ; 
-    x.longestStrChain(kosi); 
-}
